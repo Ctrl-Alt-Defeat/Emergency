505706e9f11b3f78aef5b3e0bf57b13f90d5916e
areejobaid94 <areej.obaid@yahoo.com>
2021-03-04 20:08:17 +0200

    db

--- server.js
+++ server.js
@@ -8,21 +8,22 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 const superagent = require('superagent');
 const pg = require('pg');
-const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
-const methodOverride = require('method-override')
-app.use(methodOverride('_method'))
+// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL });
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 const port = process.env.PORT || 3000;
-
-
 
 function handelError(res, error) {
     res.render('pages/error', { error: error });
 }
+
 
 client.connect().then(() => {
     app.listen(port, () => {
         console.log(`app listening at http://localhost:${port}`);
     });
 }).catch(e => {
-    console.log(e, 'errrrrroooooorrrr')
-})
+    console.log(e, 'errrrrroooooorrrr');
+});
