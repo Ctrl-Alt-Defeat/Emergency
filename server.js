c578ee8ca66c08ceac581225b79b861f95b2e347
BayanAbualhaj <bayanabualhaj1097@gmail.com>
2021-03-05 20:39:12 +0200

    signUp

--- server.js
+++ server.js
@@ -1,36 +1,148 @@
+'use strict';
 const express = require('express');
 const app = express();
-var cors = require('cors')
-require("dotenv").config();
-
-
+var cors = require('cors');
+require('dotenv').config();
 app.use(cors());
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
-
-
-
+// app.use(express.static(path.join(__dirname, 'public')));
+const methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+const port = process.env.PORT || 3000;
 const superagent = require('superagent');
+let locData = [];
+let status ='Ok';
 const pg = require('pg');
 // const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
-const client= new pg.Client({connectionString: process.env.DATABASE_URL});
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL });
 
+// ===================================================== Map ========================================================
+app.get('/map', laodMapPage);
 
-const methodOverride = require('method-override')
-app.use(methodOverride('_method'))
-const port = process.env.PORT || 3000;
+app.post('/map', getUsersLocations);
 
-
+function getUsersLocations(req, res) {
+  return getAllLocationsFromDB(req.body.work, req.body.experience).then(data => {
+    locData = data;
+    res.redirect('/map');
+  }).catch(error => {
+    console.log(error);
+  })
+};
+function getAllLocationsFromDB(work, experience) {
+  // let day = new Date().getDay();
+  // let today = new Date();
+  experience = experience || 0;
+  console.log(work, 'work')
+  return client.query('SELECT * FROM USERS left outer join schedule ON (USERS.id = schedule.user_id) WHERE type_of_work = $1 and exp >= $2', [work, experience]).then(data => {
+    return data.rows
+  }).catch(error => {
+    console.log(error)
+  });
+}
+// ===================================================================================================================
 
 function handelError(res, error) {
-    res.render('pages/error', { error: error });
+  res.render('pages/error', { error: error });
+};
+
+function laodMapPage(req, res) {
+  res.render('pages/map', { data: locData });
+};
+// ======================= Acconut page geting from database=====================
+app.get('/login/acconut/:id', handleAcconutPage);
+function handleAcconutPage(req, res) {
+  let id = req.params.id;
+  console.log(id);
+  let selectFromDB = 'SELECT * FROM users WHERE id=$1;';
+  //   console.log('DB',selectFromDB);
+  let safeValue = [id];
+  client.query(selectFromDB, safeValue).then(data => {
+    res.render('pages/accountNew', { data: data.rows[0] });
+    console.log(data.rows[0]);
+  }).catch(error => {
+    console.log(`an error occurred while getting task with ID number ${id} from DB ${error}`);
+  });
 }
 
-client.connect().then(() => {
-    app.listen(port, () => {
-        console.log(`app listening at http://localhost:${port}`);
-    });
-}).catch(e => {
-    console.log(e, 'errrrrroooooorrrr')
+// {{{{{login}}}}}________________________
+app.get('/log_Page', (req, res) => {
+  let oldStatus = status;
+  status  = 'Ok';
+  res.render('pages/login',{status:oldStatus});
 })
+
+app.post('/login', (req, res) => {
+  let quer = req.body;
+  var email = quer.email;
+  var pass = quer.password;
+  let sql = `SELECT * FROM users WHERE email = $1 and password = $2;`;
+  client.query(sql,[email,pass]).then((result) => {
+    if(result.rowCount){
+      status = 'Ok'
+      res.redirect(`/login/acconut/${result.rows[0].id}`)
+    }else{
+      status = 'Wrong Email Or Password'
+      res.redirect('/log_Page');
+    }
+  })
+})
+
+
+//===========================Sign up==================================
+
+app.post('/signUp',(req,res)=>{
+  let body=req.body;
+  let full_name= body.full_name;
+  let role=body.role;
+  let location=body.location;
+  let typeOfwork= body.type_of_work;
+  let email =body.email;
+  let userName=body.user_name;
+  let password=body.password;
+  let phoneNum= body.phone_num;
+
+  let insertQuery= 'INSERT INTO users (full_name,role,location,type_of_work,email,password,phone_num,username) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *;'
+
+  let safeValue= [full_name,role,location,typeOfwork,email,password,phoneNum,userName];
+
+
+  client.query(insertQuery,safeValue).then(data =>{
+    res.redirect(`/login/acconut/${data.rows[0].id}`);
+  }).catch(error=>{
+    res.status(500).send(`Sorry an error has accord while loading the page  ${error} `);
+  });
+});
+
+//====================================================================================
+
+
+// {{{{{}}}}}____________________________
+// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, });
+// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
+// const methodOverride = require('method-override');
+// app.use(methodOverride('_method'));
+// const port = process.env.PORT || 3000;
+
+
+// function handelError(res, error) {
+//   res.render('pages/error', { error: error });
+// }
+
+//     }).catch(error=>{
+//         console.log('you have error'+error)
+//     })
+// })
+
+client.connect().then(() => {
+  app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`);
+  });
+}).catch(e => {
+
+  console.log(e, 'errrrrroooooorrrr');
+});
