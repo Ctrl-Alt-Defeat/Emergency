d118a55522768a93b4feabe5b0c16a789b71e9d7
SalahAlawneh <salah.alawneh@yahoo.com>
2021-03-05 16:23:41 +0200

    finishing contact us page

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ function handleAcconutPage(req, res) {
   let id = req.params.id;
   console.log(id);
   let selectFromDB = 'SELECT * FROM user WHERE id=$1;';
-//   console.log('DB',selectFromDB);
+  //   console.log('DB',selectFromDB);
   let safeValue = [id];
   client.query(selectFromDB, safeValue).then(data => {
     res.render('/public/acconut', { data: data.rows[0] });
     console.log(data.rows[0]);
-  }).catch(error =>{
+  }).catch(error => {
     console.log(`an error occurred while getting task with ID number ${id} from DB ${error}`);
   });
 
@@ -37,37 +37,56 @@ function handleAcconutPage(req, res) {
 }
 
 // {{{{{login}}}}}________________________
-app.get('/',(req,res)=>{
-    res.render('pages/login')
+app.get('/', (req, res) => {
+  res.render('pages/login')
 })
 
-app.post('/loginn',(req,res)=>{
-    let quer=req.body ;
-    let SQL = 'SELECT * FROM users WHERE email = $1 and password = $2 ;' ;
-    let safevalue =[quer.username,quer.password];
-   
-    client.query(SQL,safevalue).then(data=>{
-        
-        res.render('pages/error',{data:data.rows[0]});
+app.post('/loginn', (req, res) => {
+  let quer = req.body;
+  let SQL = 'SELECT * FROM users WHERE email = $1 and password = $2 ;';
+  let safevalue = [quer.username, quer.password];
 
-    }).catch(error=>{
-        console.log('you have error'+error)
-    })
+  client.query(SQL, safevalue).then(data => {
+
+    res.render('pages/error', { data: data.rows[0] });
+
+  }).catch(error => {
+    console.log('you have error' + error)
+  })
 
 
 
 })
 
 // ======================= Contact Us Page =====================
-app.get("/contact",handleContactPage);
-function handleContactPage(req,res) {
+app.get("/contact", handleContactPage);
+function handleContactPage(req, res) {
   res.render("pages/contact")
 }
-app.post("/contact",handleContactUsForm);
-function handleContactUsForm(req,res) {
-  console.log(req.body.userName);
-  console.log(req.body.email);
-  console.log(req.body.text);
+app.post("/contact", handleContactUsForm);
+function handleContactUsForm(req, res) {
+  let userName = req.body.userName;
+  let email = req.body.email;
+  // let text = req.body.text;
+  let selectSql = "SELECT username, email FROM users;";
+  client.query(selectSql).then(table => {
+    table.rows.forEach(oneUser => {
+      if (oneUser.username === userName && oneUser.email === email) {
+
+        let SQL = `INSERT INTO contact (mess) VALUES ($1);`
+        let safeValue = [req.body.text];
+        client.query(SQL, safeValue)
+          .then(() => {
+            res.render("index")
+          }).catch(error => {
+            res.render("pages/error", { error: error });
+          })
+      }
+    });
+
+  }).catch(error => {
+    res.render("pages/error", { error: error });
+  })
 }
 
 // {{{{{}}}}}____________________________
@@ -89,5 +108,5 @@ client.connect().then(() => {
   });
 }).catch(e => {
 
-    console.log(e, 'errrrrroooooorrrr');
+  console.log(e, 'errrrrroooooorrrr');
 });
