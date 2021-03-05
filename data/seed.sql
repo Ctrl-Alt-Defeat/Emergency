3d76c1b44b0fe102d68e72578ac09c73d9ba85d0
AHMAD ALSABBAGH <a.sameer1999@gmail.com>
2021-03-05 21:06:08 +0200

    add edtion 1.3

--- data/seed.sql
+++ data/seed.sql
@@ -1,3 +1,4 @@
-INSERT INTO users (full_name, location, img, type_of_work, email, phone_num, exp, username)
-VALUES ('Ahmad Alsabbagh', 'Amman/Jordan', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-bDQfGLfL2L6AcWaVPkIGOGb79wd5SQEb0Q&usqp=CAU', 'programer', 'a.sameer1999@gmai.com', '0777045159', '3', 'sabbagh99');
+UPDATE users
+SET status = 1, role = 1
+WHERE   id=1;
 
