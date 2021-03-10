13328d3aa66cbcaf4b7ea3b4c0f888e29344683e
areejobaid94 <areej.obaid@yahoo.com>
2021-03-10 18:37:42 +0200

    done

--- data/herokuDB.sql
+++ data/herokuDB.sql
@@ -1,1 +1,3 @@
 SELECT * FROM USERS;
+
+INSERT INTO  users(full_name,role,location,img,type_of_work,email,password,phone_num,status,exp,username) VALUES ('Hanaa','3','35.868519,31.897918','https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80','Carpenter','hanaa.maratouq@yahoo.com','1234','070966666','1',2,'Hanaa');
